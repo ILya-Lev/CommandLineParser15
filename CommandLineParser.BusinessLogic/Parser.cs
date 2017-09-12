2363bfbd499bda2d1d3802e08324a0b44da6c941
ILya-Lev <lis2889@gmail.com>
2017-09-12 18:58:17 +0300

    Add project files.

---
+++ CommandLineParser.BusinessLogic/Parser.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommandLineParser.BusinessLogic
+{
+	public class Parser
+	{
+		private const char KeyToken = '-';
+		private const char Space = ' ';
+		private const char Quote = '\"';
+
+		public Dictionary<string, string> Arguments(string inputString)
+		{
+			var claims = SplitIntoClaims(inputString).ToList();
+
+			var arguments = new Dictionary<string, string>();
+			for (int i = 0; i < claims.Count; i++)
+			{
+				if (i + 1 < claims.Count)
+				{
+					if (claims[i].IsKey && claims[i + 1].IsValue)   //operation with argument
+					{
+						arguments.Add(claims[i].Body, claims[i+1].Body);
+						i++;
+						continue;
+					}
+				}
+
+				if (claims[i].IsKey)                                //operation without argument
+					arguments.Add(claims[i].Body, null);
+				else
+					throw new Exception("Argument should not be met without its operation name! Details: "
+										+ $"isKey = {claims[i].IsKey}; "
+										+ $"isValue = {claims[i].IsValue}; "
+										+ $"body = {claims[i].Body}.");
+			}
+			return arguments;
+		}
+
+		private IEnumerable<Claim> SplitIntoClaims(string inputString)
+		{
+			for (int i = 0; i < inputString.Length; i++)
+			{
+				if (inputString[i] == Quote)
+				{
+					var claimBody = GetValueInQuotes(ref i, inputString);
+					yield return new Claim { IsKey = false, IsValue = true, Body = claimBody };
+					continue;
+				}
+
+				if (inputString[i] == KeyToken)
+				{
+					var claimBody = GetKey(inputString, ref i);
+					yield return new Claim { IsKey = true, IsValue = false, Body = claimBody };
+					continue;
+				}
+
+				if (!char.IsWhiteSpace(inputString[i]))
+				{
+					var claimBody = GetValue(inputString, ref i);
+					yield return new Claim { IsKey = false, IsValue = true, Body = claimBody };
+				}
+			}
+		}
+
+		private static string GetValueInQuotes(ref int i, string inputString)
+		{
+			var nextQuote = inputString.IndexOf(Quote, i + 1);
+			if (nextQuote < i)
+				throw new Exception($"There is no closing double quote symbol after position {i}.");
+			var claimBody = inputString.Substring(i + 1, nextQuote - i - 1);
+			i = nextQuote;
+			return claimBody;
+		}
+
+		private static string GetKey(string inputString, ref int i)
+		{
+			string claimBody;
+			var keyEndsAt = inputString.IndexOf(Space, i);
+			if (keyEndsAt < 0)
+			{
+				claimBody = inputString.Substring(i + 1);
+				i = inputString.Length;
+			}
+			else
+			{
+				claimBody = inputString.Substring(i + 1, keyEndsAt - i - 1);
+				i = keyEndsAt;
+			}
+			return claimBody;
+		}
+
+		private static string GetValue(string inputString, ref int i)
+		{
+			string claimBody;
+			var valueEndsAt = inputString.IndexOf(Space, i);
+			if (valueEndsAt < 0)
+			{
+				claimBody = inputString.Substring(i);
+				i = inputString.Length;
+			}
+			else
+			{
+				claimBody = inputString.Substring(i, valueEndsAt - i);
+				i = valueEndsAt;
+			}
+			return claimBody;
+		}
+	}
+
+	internal class Claim
+	{
+		public bool IsKey { get; set; }
+		public bool IsValue { get; set; }
+		public string Body { get; set; }
+	}
+}
---
+++ CommandLineParser.UnitTests/ParserTests.cs
@@ -0,0 +1,85 @@
+﻿using CommandLineParser.BusinessLogic;
+using FluentAssertions;
+using Xunit;
+
+namespace CommandLineParser.UnitTests
+{
+	public class ParserTests
+	{
+		[Theory]
+		[InlineData("-help", "help")]
+		[InlineData("-h", "h")]
+		[InlineData("-somethingLongEnoughToBeAwfull", "somethingLongEnoughToBeAwfull")]
+		[InlineData("-help ", "help")]
+		[InlineData("-he-lp ", "he-lp")]
+		[InlineData(@"-he""lp ", @"he""lp")]
+		[InlineData("-", "")]
+		public void Arguments_OneArgumentWithoutValue_FindOneArgument(string input, string commandName)
+		{
+			var args = new Parser().Arguments(input);
+
+			args.Should().HaveCount(1);
+			args.Should().ContainKey(commandName);
+			args.Should().ContainValue(null);
+		}
+
+		[Theory]
+		[InlineData("-file -folder", "file", "folder")]
+		[InlineData("-file    -folder   ", "file", "folder")]
+		public void Arguments_TwoArgumentWithoutValue_FindTwoArguments(string input, string firstKey, string secondKey)
+		{
+			var args = new Parser().Arguments(input);
+
+			args.Should().HaveCount(2);
+			args.Should().ContainKey(firstKey);
+			args.Should().ContainKey(secondKey);
+		}
+
+		[Theory]
+		[InlineData("-file story.txt", "file", "story.txt")]
+		[InlineData("-f story.txt", "f", "story.txt")]
+		[InlineData("-file story.txt  ", "file", "story.txt")]
+		[InlineData("-f     story.txt   ", "f", "story.txt")]
+		[InlineData("-file st-ory.txt", "file", "st-ory.txt")]
+		[InlineData("-file s", "file", "s")]
+		[InlineData("- s", "", "s")]                            //todo: not sure this should be a valid case
+		public void Arguments_OneArgumentAndValue_FindArgumentAndValue(string input, string key, string value)
+		{
+			var args = new Parser().Arguments(input);
+
+			args.Should().HaveCount(1);
+			args.Should().ContainKey(key);
+			args.Should().ContainValue(value);
+		}
+
+		[Theory]
+		[InlineData("-file story.txt -folder ProgramFiles", "file", "story.txt", "folder", "ProgramFiles")]
+		[InlineData("-f story.txt -folder ProgramFiles", "f", "story.txt", "folder", "ProgramFiles")]
+		[InlineData("-file story.txt     -folder ProgramFiles", "file", "story.txt", "folder", "ProgramFiles")]
+		[InlineData("-file sto-ry.txt -folder Program-Files", "file", "sto-ry.txt", "folder", "Program-Files")]
+		[InlineData("-file s -folder ProgramFiles", "file", "s", "folder", "ProgramFiles")]
+		[InlineData("-file story.txt -folder p", "file", "story.txt", "folder", "p")]
+		[InlineData("-file story.txt -folder  ", "file", "story.txt", "folder", null)]
+		public void Arguments_TwoArgumentsAndValue_FindAll(string input, string firstKey, string firstValue, string secondKey, string secondValue)
+		{
+			var args = new Parser().Arguments(input);
+
+			args.Should().HaveCount(2);
+			args.Should().ContainKey(firstKey);
+			args.Should().ContainValue(firstValue);
+			args.Should().ContainKey(secondKey);
+			args.Should().ContainValue(secondValue);
+		}
+
+		[Theory]
+		[InlineData(@"-f ""C:\User\Vasya Pupkin\story.txt""", "f", @"C:\User\Vasya Pupkin\story.txt")]
+		public void Arguments_QuotedValue_FindsValue(string input, string key, string value)
+		{
+			var args = new Parser().Arguments(input);
+
+			args.Should().HaveCount(1);
+			args.Should().ContainKey(key);
+			args.Should().ContainValue(value);
+		}
+	}
+}
